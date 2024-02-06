331e28f8fa4085519f7a1e4df593655008239999
Britnay Gore <britnaygore268@gmail.com>
2024-02-05 22:10:02 -0500

    Created renderToDom function in main.js file

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("Test")
+
+// Utility Functions
+const renderToDom = (divId, htmlToRender) => {
+  const selectedId = document.querySelector(divId);
+  selectedId.innerHTML = htmlToRender;
+};
